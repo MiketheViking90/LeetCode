986d9d6d3c0704e6617cdaebc87ba54417ff8300
Sili Wen <siliwen@gmail.com>
2017-01-04 10:11:17 -0800

    Change access to public in TreeNode

---
+++ src/MaxDepthOfBinaryTree.java
@@ -0,0 +1,12 @@
+import domain.TreeNode;
+
+public class MaxDepthOfBinaryTree {
+
+    public int maxDepth(TreeNode root) {
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        return 1;
+    }
+
+}
