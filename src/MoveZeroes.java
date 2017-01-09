824327c2966ec189d6a7f0dcf6279273ce178d3e
Sili Wen <siliwen@gmail.com>
2017-01-09 09:24:01 -0800

    Reverse list and move zeros

--- src/MoveZeroes.java
+++ src/MoveZeroes.java
@@ -3,34 +3,49 @@ import java.util.Arrays;
 public class MoveZeroes {
 
     public void moveZeroes(int[] nums) {
-        int n = nums.length;
 
-        int i = 0;
-        while (i<n) {
-            while (nums[i] != 0) {
-                i++;
-            }
+        int curIndex = getNextZeroIndex(0, nums);
+        int nextIndex = getNextNonZeroIndex(0, nums);
 
-            int j = i;
-            while (nums[j] == 0 && j<n) {
-                j++;
-            }
+        while (nextIndex < nums.length) {
+            nums[curIndex] = nums[nextIndex];
+            nums[nextIndex] = 0;
 
             System.out.println(Arrays.toString(nums));
-            System.out.println("\t" + i);
-            System.out.println("\t" + j);
-            nums[i] = nums[j];
-            nums[j] = 0;
+            curIndex = getNextZeroIndex(curIndex + 1, nums);
+            nextIndex = getNextNonZeroIndex(nextIndex + 1, nums);
+
+            if (curIndex == -1 || nextIndex == -1) {
+                return;
+            }
+            System.out.println("cur: " + curIndex + ": " + nums[curIndex] + " next: " + nextIndex + ": " + nums[nextIndex]);
         }
     }
 
-    public static void main(String[] args) {
-
-        MoveZeroes mz = new MoveZeroes();
-        int[] nums = {0, 1, 0, 3, 12};
-
-        mz.moveZeroes(nums);
-        System.out.println(Arrays.toString(nums));
+    private int getNextNonZeroIndex(int i, int[] nums) {
+        while (nums[i] == 0) {
+            if (i == nums.length) {
+                return -1;
+            }
+            i++;
+        }
+        return i;
     }
 
+    private int getNextZeroIndex(int i, int[] nums) {
+        while (nums[i] != 0) {
+            if (i >= nums.length) {
+                return -1;
+            }
+            i++;
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        MoveZeroes mz = new MoveZeroes();
+
+        int[] nums = {0, 1, 0, 3, 12};
+        mz.moveZeroes(nums);
+    }
 }
