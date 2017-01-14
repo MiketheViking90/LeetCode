371dc1f2237f3c56055c2ffdedbd7d2f4fe30825
Sili Wen <siliwen@gmail.com>
2017-01-14 08:34:16 -0800

    Sum int as array

--- src/BitManipulation.java
+++ src/BitManipulation.java
@@ -17,22 +17,45 @@ public class BitManipulation {
         return num & mask;
     }
 
+    int swapBits(int num, int i, int j) {
+        int mask = (1 << i ) | (1 << j);
+        System.out.println(toBinaryString(mask));
+        return num ^= mask;
+    }
+
+    int complement(int n) {
+        int maxSetBit = 0;
+        int i = 32;
+        while (i > 0) {
+            if ((n >> i) == 1) {
+                maxSetBit = i;
+            }
+            i--;
+        }
+        int mask = (1 << (maxSetBit + 1)) - 1;
+        System.out.println(toBinaryString(mask));
+        return n ^ mask;
+    }
+
     int mask(int n) {
         return (~0 << n);
     }
 
     public static void main(String[] args) {
         BitManipulation bm = new BitManipulation();
-        int val = 13;
-        System.out.println(toBinaryString(val));
+//        int val = 13;
+//        System.out.println(toBinaryString(val));
+//
+//        int setBit = bm.setBit(valasdfafda, 1);
+//        System.out.println(toBinaryString(setBit));
+//
+//        int mask = bm.mask(3);
+//        System.out.println(toBinaryString(mask));
+//asdfadsf
+//        int clearBit = bm.clearBit(val, 2);
+//        System.out.println(toBinaryString(clearBit));
 
-        int setBit = bm.setBit(val, 1);
-        System.out.println(toBinaryString(setBit));
-
-        int mask = bm.mask(3);
-        System.out.println(toBinaryString(mask));
-
-        int clearBit = bm.clearBit(val, 2);
-        System.out.println(toBinaryString(clearBit));
+        System.out.println(toBinaryString(77));
+        System.out.println(toBinaryString(bm.complement(77)));
     }
 }
