18a90d1e0122e2bb5ba7a63eedee374a90ab3433
Sili Wen <siliwen@gmail.com>
2017-08-01 20:38:39 -0700

    BFS for trees using property of BFS to track layer

---
+++ src/algorithms/FindBottomLeftTreeValue.java
@@ -0,0 +1,32 @@
+package algorithms;
+
+import domain.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FindBottomLeftTreeValue {
+
+    int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> visitQ = new LinkedList<>();
+        visitQ.add(root);
+        int btmLeft = root.val;
+
+        while (!visitQ.isEmpty()) {
+            int depthCnt = visitQ.size();
+            btmLeft = visitQ.peek().val;
+            for (int i = 0; i < depthCnt; i++) {
+                TreeNode visit = visitQ.poll();
+
+                if (visit.left != null) {
+                    visitQ.offer(visit.left);
+                }
+                if (visit.right!= null) {
+                    visitQ.offer(visit.right);
+                }
+            }
+        }
+
+        return btmLeft;
+    }
+}
