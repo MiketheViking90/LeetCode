95b9516111195885b592693ab041e803d147d395
Sili Wen <siliwen@gmail.com>
2017-08-01 20:26:44 -0700

    correct indices

--- src/algorithms/PalindromicSubstrings.java
+++ src/algorithms/PalindromicSubstrings.java
@@ -2,7 +2,7 @@ package algorithms;
 
 public class PalindromicSubstrings {
 
-    int countPalindromes(String str) {
+    int countSubstrings(String str) {
         int n = str.length();
         boolean[][] M = new boolean[n][n];
 
@@ -21,13 +21,14 @@ public class PalindromicSubstrings {
             }
         }
 
-        for (int len = 3; len < n; len++) {
-            for (int i = 0; i < n - len; i++) {
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
                 int j = (i + len - 1);
                 char c = str.charAt(i);
                 char c1 = str.charAt(j);
-
+                System.out.println(i + ", " + j);
                 if (M[i+1][j-1] && (c == c1)) {
+                    System.out.println(str.substring(i, j+1));
                     M[i][j] = true;
                     cnt++;
                 }
@@ -40,7 +41,7 @@ public class PalindromicSubstrings {
     public static void main(String[] args) {
         PalindromicSubstrings ps = new PalindromicSubstrings();
 
-        int cnt = ps.countPalindromes("abccbaaaabca");
+        int cnt = ps.countSubstrings("aaa");
         System.out.println(cnt);
     }
 }
