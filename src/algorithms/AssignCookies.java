bcb9de732636bbdea0305e2c02631f6e6ffc4990
Sili Wen <siliwen@gmail.com>
2017-08-02 20:43:33 -0700

    Assign cookies and relative ranks

---
+++ src/algorithms/AssignCookies.java
@@ -0,0 +1,32 @@
+package algorithms;
+
+import java.util.Arrays;
+
+public class AssignCookies {
+
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        if (g.length == 0 || s.length == 0) {
+            return 0;
+        }
+
+        int i = g.length - 1;
+        int j = s.length - 1;
+
+        int cnt = 0;
+        while (i >= 0 && j >= 0) {
+            if (s[j] >= g[i]) {
+                j--;
+                i--;
+                cnt++;
+            } else {
+                i--;
+            }
+        }
+
+        return cnt;
+    }
+
+}
---
+++ src/algorithms/RelativeRanks.java
@@ -0,0 +1,41 @@
+package algorithms;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class RelativeRanks {
+
+    public String[] findRelativeRanks(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(3);
+        for (int n : nums) {
+            pq.add(n);
+        }
+
+        Map<Integer, String> valToMedal = new HashMap<>();
+        String[] medals = {"Gold Medal", "Silver Medal", "Bronze Medal"};
+        for (int i = 0; i < 3; i++) {
+            int val = pq.poll();
+            valToMedal.put(val, medals[i]);
+        }
+
+        String[] ranks = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            String rank = valToMedal.getOrDefault(n, String.valueOf(n));
+            ranks[i] = rank;
+        }
+
+        return ranks;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5, 4, 3, 2, 1};
+
+        RelativeRanks rr = new RelativeRanks();
+        String[] ranks = rr.findRelativeRanks(nums);
+
+        System.out.println(Arrays.toString(ranks));
+    }
+}
