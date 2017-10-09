8ec851baa54ed6dda3c8839081ebef731c614e9e
Sili Wen <siliwen@amazon.com>
2017-10-09 15:35:04 -0700

    Root to leaf sum

---
+++ src/algorithms/PathSum.java
@@ -0,0 +1,39 @@
+package algorithms;
+
+import domain.TreeNode;
+
+public class PathSum {
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+
+        return hasPathSum_R(root, 0, sum);
+    }
+
+    private boolean hasPathSum_R(TreeNode root, int curSum, int target) {
+        if (root == null) {
+            return false;
+        }
+
+        int newSum = curSum + root.val;
+        if (root.left == null && root.right == null) {
+            return newSum == target;
+        }
+
+        return hasPathSum_R(root.left, newSum, target) ||
+                hasPathSum_R(root.right, newSum, target);
+    }
+
+    public static void main(String[] args) {
+        //TreeNode root = TreeNode.makeTree();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+
+        PathSum ps = new PathSum();
+        boolean hasSum = ps.hasPathSum(root, 1);
+        System.out.println(hasSum);
+    }
+
+}
