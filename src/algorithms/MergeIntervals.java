8824a870fa39fa6448a56cc08b4075d4e47d07ad
Sili Wen <siliwen@amazon.com>
2017-10-17 12:17:41 -0700

    Complete intervals

--- src/algorithms/MergeIntervals.java
+++ src/algorithms/MergeIntervals.java
@@ -1,14 +1,22 @@
 package algorithms;
 
+import lombok.ToString;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class MergeIntervals {
 
-    class Interval {
+    @ToString
+    public static class Interval {
         int start;
         int end;
+
+        public Interval(int s, int e) {
+            this.start = s;
+            this.end = e;
+        }
     }
 
     public List<Interval> merge(List<Interval> intervals) {
@@ -16,6 +24,7 @@ public class MergeIntervals {
             return i1.start - i2.start;
         };
 
+        if (intervals.si)
         intervals.sort(startComparator);
         List<Interval> res = new ArrayList<>();
 
@@ -26,14 +35,26 @@ public class MergeIntervals {
             } else {
                 int curEnd = curInterval.end;
                 if (interval.start <= curEnd) {
-                    curEnd = Math.max(curEnd, interval.end);
+                    curInterval.end = Math.max(curEnd, interval.end);
                 } else {
                     res.add(curInterval);
-                    curInterval = null;
+                    curInterval = interval;
                 }
             }
         }
+        res.add(curInterval);
         return res;
     }
 
+    public static void main(String[] args) {
+        List<Interval> intervals = new ArrayList<>();
+        intervals.add(new Interval(1, 3));
+        intervals.add(new Interval(2, 6));
+        intervals.add(new Interval(8, 10));
+        intervals.add(new Interval(15, 18));
+
+        MergeIntervals mi = new MergeIntervals();
+        List<Interval> res = mi.merge(intervals);
+        System.out.println(res);
+    }
 }
