73805b474ec57846df12720019c3e5e04f603e3e
Sili Wen <siliwen@gmail.com>
2017-10-23 20:19:56 -0700

    Correct palin

--- src/algorithms/ValidPalindrome2.java
+++ src/algorithms/ValidPalindrome2.java
@@ -19,16 +19,27 @@ public class ValidPalindrome2 {
         String skipLeft = s.substring(i+1, j + 1);
         String skipRight = s.substring(i, j);
 
-        if (validPalindrome(skipLeft) || validPalindrome(skipRight)) {
+        if (isPalin(skipLeft) || isPalin(skipRight)) {
             return true;
         }
         return false;
     }
 
+    private boolean isPalin(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i<j && s.charAt(i) == s.charAt(j)) {
+            i++;
+            j--;
+        }
+
+        return (j <= i);
+    }
+
     public static void main(String[] args) {
         ValidPalindrome2 vp2 = new ValidPalindrome2();
 
-        String str = "raceczar";
+        String str = "racecarz";
         boolean b = vp2.validPalindrome(str);
         System.out.println(b);
     }
