3d493674da92c7fb901a847984d1b1695d38e9ff
Sili Wen <siliwen@amazon.com>
2018-12-20 15:58:08 -0800

    MinAddToMakeValid

---
+++ src/eighteen/algo/MinAddToMakeValid.java
@@ -0,0 +1,31 @@
+package eighteen.algo;
+
+public class MinAddToMakeValid {
+
+    public int minAddToMakeValid(String s) {
+        int n = 0;
+        int lVal = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                lVal++;
+            } else {
+                if (lVal > 0) {
+                    lVal--;
+                } else {
+                    n++;
+                }
+            }
+        }
+
+        return n + lVal;
+    }
+
+    public static void main(String[] args) {
+        String s = "()";
+        MinAddToMakeValid ma = new MinAddToMakeValid();
+        int n = ma.minAddToMakeValid(s);
+        System.out.println(n);
+    }
+
+}
