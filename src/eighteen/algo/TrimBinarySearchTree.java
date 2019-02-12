29f533b6b5fec82cd85b449088bc4848605f0959
Sili Wen <siliwen@gmail.com>
2019-02-11 20:37:37 -0800

    CustomSort and TrimBST

---
+++ src/eighteen/algo/TrimBinarySearchTree.java
@@ -0,0 +1,22 @@
+package eighteen.algo;
+
+import seventeen.domain.TreeNode;
+
+public class TrimBinarySearchTree {
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) {
+            return null;
+        }
+
+        int v = root.val;
+        if (v >= L && v <= R) {
+            root.left = trimBST(root.left, L, R);
+            root.right = trimBST(root.right, L, R);
+            return root;
+        } else if (v < L) {
+            return trimBST(root.right, L, R);
+        } else {
+            return trimBST(root.left, L, R);
+        }
+    }
+}
