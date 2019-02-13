7c367474f464289db8e4c6399efe052786a866f4
Sili Wen <siliwen@amazon.com>
2019-02-12 17:20:24 -0800

    Single Number, Daily Temperatures by stack with index

--- src/eighteen/algo/SingleNumber.java
+++ src/eighteen/algo/SingleNumber.java
@@ -1,4 +1,11 @@
 package eighteen.algo;
 
 public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int x = 0;
+        for (int n : nums) {
+            x ^= n;
+        }
+        return x;
+    }
 }
