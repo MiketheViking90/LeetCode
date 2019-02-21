ed982535539b4d51007be5784f2fb49a93beb365
Sili Wen <siliwen@amazon.com>
2019-02-20 17:28:25 -0800

    min cost for tickets - TODO, DP min falling path sum - DFS, exceed time bottom left value of tree - keep state in class validate stack seq - recreate stack avg lvls in BT - bfs

--- src/eighteen/algo/MinimumFallingPathSum.java
+++ src/eighteen/algo/MinimumFallingPathSum.java
@@ -4,10 +4,17 @@ public class MinimumFallingPathSum {
     int minSum = Integer.MAX_VALUE;
     int M;
     int N;
+    int[][] MEM;
 
     public int minFallingPathSum(int[][] A) {
         M = A.length;
         N = A[0].length;
+        MEM = new int[M][N];
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                MEM[i][j] = Integer.MAX_VALUE;
+            }
+        }
 
         for (int i = 0; i < N; i++) {
             dfs(0, i, 0, A);
@@ -15,30 +22,24 @@ public class MinimumFallingPathSum {
         return minSum;
     }
 
-    private void dfs(int m, int n, int sum, int[][] A) {
+    private int dfs(int m, int n, int sum, int[][] A) {
         if (m < 0 || m >= M || n < 0 || n >= N) {
-            return;
+            return Integer.MAX_VALUE;
         }
-
         int cur = A[m][n];
-        sum += cur;
-        if (m == M-1) {
-            minSum = Math.min(minSum, sum);
-            return;
+        if (MEM[m][n] != Integer.MAX_VALUE) {
+            return cur + MEM[m][n];
+        } else {
+            sum += cur;
+            if (m == M - 1) {
+                minSum = Math.min(minSum, sum);
+            }
+            int a = dfs(m + 1, n - 1, sum, A);
+            int b = dfs(m + 1, n, sum, A);
+            int c = dfs(m + 1, n + 1, sum, A);
+            int min = Math.min(a, Math.min(b, c));
+            MEM[m][n] = min;
+            return min;
         }
-        dfs(m+1, n-1, sum, A);
-        dfs(m+1, n, sum, A);
-        dfs(m+1, n+1, sum, A);
-    }
-
-    public static void main(String[] args) {
-        MinimumFallingPathSum mfps = new MinimumFallingPathSum();
-        int[][] matrix = {
-            {1,2,3},
-            {4,5,6},
-            {7,8,9}
-        };
-        int v = mfps.minFallingPathSum(matrix);
-        System.out.println(v);
     }
 }
