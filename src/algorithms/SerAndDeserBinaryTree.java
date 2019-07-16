644d278ecf0b978b91119b898f9e66b498cc834e
Jim Wen <siliwen@amazon.com>
2019-07-15 19:57:02 -0700

    TODO figure out deser a tree

--- src/algorithms/SerAndDeserBinaryTree.java
+++ src/algorithms/SerAndDeserBinaryTree.java
@@ -2,6 +2,11 @@ package algorithms;
 
 import archive.domain.TreeNode;
 
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
 public class SerAndDeserBinaryTree {
 
     public String serialize(TreeNode root) {
@@ -27,12 +32,35 @@ public class SerAndDeserBinaryTree {
     }
 
     public TreeNode deserialize(String data) {
-        return null;
+        Deque<String> nodeStack = new LinkedList<>();
+        List<String> dataList = Arrays.asList(data.split(", "));
+        for (String s : dataList) {
+            nodeStack.push(s);
+        }
+        return buildTree(nodeStack);
+    }
+
+    private TreeNode buildTree(Deque<String> nodeStack) {
+        if (nodeStack.isEmpty()) {
+            return null;
+        }
+        String top = nodeStack.pop();
+        if (top.equals("")) {
+            return null;
+        }
+        TreeNode node = new TreeNode(Integer.parseInt(top));
+        node.right = buildTree(nodeStack);
+        node.left = buildTree(nodeStack);
+        return node;
     }
 
     public static void main(String[] args) {
         SerAndDeserBinaryTree ser = new SerAndDeserBinaryTree();
         String serial = ser.serialize(TreeNode.makeTree());
-        System.out.println(serial);
+        //System.out.println(serial);
+
+        TreeNode node = ser.deserialize(serial);
+        TreeNode.print(node);
+        TreeNode.print(TreeNode.makeTree());
     }
 }
