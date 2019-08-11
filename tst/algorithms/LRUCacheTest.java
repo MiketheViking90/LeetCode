12530706e2beafc99ab66bbad71e7377d63e7d03
Sili Wen <sili@SpaceGrey.local>
2019-08-10 20:14:42 -0700

    tst for LRU Cache

--- tst/algorithms/LRUCacheTest.java
+++ tst/algorithms/LRUCacheTest.java
@@ -1,5 +1,54 @@
+package algorithms;
+
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 public class LRUCacheTest {
 
+    @Test
+    public void testPut() {
+        LRUCache lru = new LRUCache();
+        lru.put("a", 1);
+        System.out.println("a");
+        printList(lru);
+
+        lru.put("b", 2);
+        System.out.println("b -> a");
+        printList(lru);
+
+        lru.put("c", 3);
+        System.out.println("c -> b -> a");
+        printList(lru);
+
+        System.out.println("get");
+        lru.get("a");
+        System.out.println("a -> c -> b");
+        printList(lru);
+
+        lru.put("d", 4);
+        System.out.println("d -> a -> c");
+        printList(lru);
+
+        lru.put("e", 5);
+        System.out.println("e -> d -> a");
+        printList(lru);
+    }
+
+    private void printList(LRUCache lru) {
+        int size = lru.getSize();
+        DoubleLinkedNode node = lru.getHead();
+
+        for (int i = 0; i < 5; i++) {
+            if (node != null) {
+                //System.out.println(node);
+                System.out.println(node.k + ", " + node.v);
+                node = node.next;
+            }
+        }
+    }
+
+    @Test
+    public void testGet() {
+    }
 }
